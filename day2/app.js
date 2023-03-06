1e1f011c7b00271bc5adf35e9b30965aac7ff5f0
박준영 <114470248+JunYoungee@users.noreply.github.com>
2023-03-06 12:49:22 +0900

    day3

--- day2/app.js
+++ day2/app.js
@@ -8,7 +8,7 @@ const createRouter = require('./Router/auth.router')
 app.use(express.json())
 app.use('/auth', createRouter);
 
-mongoose.connect("mongodb+srv://wnsdud96330:ajtwodl123@cluster0.uznhppl.mongodb.net/?retryWrites=true&w=majority", {dbName: "second"})
+mongoose.connect("mongodb+srv://wnsdud96330:<password>@cluster0.uznhppl.mongodb.net/?retryWrites=true&w=majority", {dbName: "second"})
     .then((result)=>{
         console.log('서버가 실행되었습니다.')
         app.listen(8080);
